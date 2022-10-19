b7b3a10c2ad85193bbec04e92e647c7ec6e0f122
Yael <yael.mattmann@gmail.com>
2022-10-19 13:05:14 +0100

    Added all config files

--- src/test/java/ie/atu/CalcTest.java
+++ src/test/java/ie/atu/CalcTest.java
@@ -1,4 +1,22 @@
 package ie.atu;
 
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class CalcTest {
+    @Test
+    void firstTest(){
+
+    }
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
 }
